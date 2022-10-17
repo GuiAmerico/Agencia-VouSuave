815990e2842cf6f23fe5950d0c11ca802eae85d7
GuiAmerico <guilhermao127@gmail.com>
2022-10-17 18:09:32 -0300

    criacao e configuraçao da entity Passagem

---
+++ agencia_vousuave/src/main/java/com/agencia/vousuave/entity/Passagem.java
@@ -0,0 +1,40 @@
+package com.agencia.vousuave.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Passagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotEmpty(message = "Campo Origem é obrigatório")
+	@Column(length = 110)
+	private String origem;
+
+	@NotEmpty(message = "Campo Destino é obrigatório")
+	@Column(length = 110)
+	private String destino;
+	
+	@NotEmpty(message = "Campo Preço é obrigatório")
+	@Column
+	private double preco;
+	
+	@NotEmpty(message = "Campo Desconto é obrigatório")
+	@Column
+	private double desconto;
+	
+	@NotEmpty(message = "Campo Caminho da Imagem é obrigatório")
+	@Column(name = "caminho")
+	private String caminhoImagem;
+	
+	@Column(name = "id_tipo")
+	@NotEmpty(message = "Campo Tipo de Passagem é obrigatório")
+	private TiposPassagem tiposPassagem;
+}
