f4fda09c6daaee27b0e11e59196b6ea55025842a
GuiAmerico <guilhermao127@gmail.com>
2022-10-17 16:52:33 -0300

    iniciando projeto

---
+++ agencia_vousuave/src/main/java/com/agencia/vousuave/AgenciaVousuaveApplication.java
@@ -0,0 +1,13 @@
+package com.agencia.vousuave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaVousuaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaVousuaveApplication.class, args);
+	}
+
+}
