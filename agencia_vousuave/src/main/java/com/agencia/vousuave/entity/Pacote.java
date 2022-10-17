a1add1293642a647a8c0028542ee5428ed9ff86f
GuiAmerico <guilhermao127@gmail.com>
2022-10-17 19:48:55 -0300

    criando e configurando entity Pacote

--- agencia_vousuave/src/main/java/com/agencia/vousuave/entity/Pacote.java
+++ agencia_vousuave/src/main/java/com/agencia/vousuave/entity/Pacote.java
@@ -1,5 +1,49 @@
 package com.agencia.vousuave.entity;
 
-public class Pacote {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotEmpty;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Pacote {
+		
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@NotEmpty(message = "Campo Destino é obrigatório")
+	@Column(length = 40)
+	@OneToOne
+	@JoinColumn
+	private Passagem destino;
+	
+	@NotEmpty(message = "Campo Preço é obrigatório")
+	@Column
+	private double preco;
+	
+	@NotEmpty(message = "Campo Diarias é obrigatório")
+	@Column(name = "diaria")
+	private int diarias;
+	
+	@NotEmpty(message = "Campo Hotel é obrigatório")
+	@Column
+	private String hotel;
+	
+	@NotEmpty(message = "Campo Guia de Turismo é obrigatório")
+	@Column(name = "guia_turismo")
+	private boolean guia;
+	
+	@NotEmpty(message = "Campo Internacional é obrigatório")
+	@Column
+	private boolean internacional;
+	
 }
