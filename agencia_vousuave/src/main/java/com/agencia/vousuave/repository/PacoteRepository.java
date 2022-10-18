e3904bf41eeb9946ad6ecd867a4d939ba9900823
GuiAmerico <guilhermao127@gmail.com>
2022-10-18 17:05:50 -0300

    criando interface PacoteRepository

---
+++ agencia_vousuave/src/main/java/com/agencia/vousuave/repository/PacoteRepository.java
@@ -0,0 +1,9 @@
+package com.agencia.vousuave.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.agencia.vousuave.entity.Pacote;
+
+public interface PacoteRepository extends JpaRepository<Pacote, Integer>{
+
+}
