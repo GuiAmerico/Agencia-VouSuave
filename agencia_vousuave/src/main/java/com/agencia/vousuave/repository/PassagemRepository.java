5db1d5f2c662a89146ffd1835dbb715830197ca4
GuiAmerico <guilhermao127@gmail.com>
2022-10-18 17:08:20 -0300

    criando interface PassagemRepository

---
+++ agencia_vousuave/src/main/java/com/agencia/vousuave/repository/PassagemRepository.java
@@ -0,0 +1,10 @@
+package com.agencia.vousuave.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.agencia.vousuave.entity.Passagem;
+
+public interface PassagemRepository extends JpaRepository<Passagem, Integer>{
+	
+	
+}
