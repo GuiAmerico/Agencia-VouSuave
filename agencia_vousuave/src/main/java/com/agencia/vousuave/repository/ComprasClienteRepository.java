1b9ff5564dcb10ab90acad825f78515157d75ecb
GuiAmerico <guilhermao127@gmail.com>
2022-10-18 17:07:06 -0300

    alterando ComprasClientes de classe para interface

--- agencia_vousuave/src/main/java/com/agencia/vousuave/repository/ComprasClienteRepository.java
+++ agencia_vousuave/src/main/java/com/agencia/vousuave/repository/ComprasClienteRepository.java
@@ -1,5 +1,9 @@
 package com.agencia.vousuave.repository;
 
-public class ComprasClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.agencia.vousuave.entity.ComprasCliente;
+
+public interface ComprasClienteRepository extends JpaRepository<ComprasCliente, Integer>{
 
 }
