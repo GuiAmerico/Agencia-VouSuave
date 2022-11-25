c2fac5a3ff18d2894444a93341cdafafddf2a30f
GuiAmerico <guilhermao127@gmail.com>
2022-11-25 16:57:23 -0300

    :sparkles: feat: add controller de auth

---
+++ agencia_vousuave/src/main/java/com/agencia/vousuave/controller/AuthController.java
@@ -0,0 +1,107 @@
+package com.agencia.vousuave.controller;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.agencia.vousuave.entity.Role;
+import com.agencia.vousuave.entity.Usuario;
+import com.agencia.vousuave.enums.ERole;
+import com.agencia.vousuave.repository.RoleRepository;
+import com.agencia.vousuave.repository.UsuarioRepository;
+import com.agencia.vousuave.request.LoginRequest;
+import com.agencia.vousuave.request.SignupRequest;
+import com.agencia.vousuave.response.JwtResponse;
+import com.agencia.vousuave.security.jwt.JwtUtils;
+import com.agencia.vousuave.service.impl.UserDetailsImpl;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	UsuarioRepository userRepository;
+
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+		List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
+				.collect(Collectors.toList());
+
+		return ResponseEntity
+				.ok(new JwtResponse(jwt, userDetails.getId(), userDetails.getNome(), userDetails.getEmail(), roles));
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+		Usuario u = new Usuario(signupRequest.getNome(), signupRequest.getEmail(), signupRequest.getCpf(), signupRequest.getCelular(),
+				signupRequest.getDataNascimento(), encoder.encode(signupRequest.getSenha()));
+
+		Set<String> strRoles = signupRequest.getRole();
+		Set<Role> roles = new HashSet<>();
+
+		if (strRoles == null) {
+			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+			roles.add(userRole);
+		} else {
+			strRoles.forEach(role -> {
+				switch (role) {
+				case "admin":
+					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(adminRole);
+
+					break;
+
+				default:
+					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(userRole);
+				}
+			});
+		}
+
+		u.setRoles(roles);
+		userRepository.save(u);
+
+		return ResponseEntity.ok("User registered successfully!");
+	}
+
+}
