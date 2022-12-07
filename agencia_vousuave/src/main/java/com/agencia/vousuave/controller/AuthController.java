c8c01b11c00029a070030c811a0ef1796f008801
GuiAmerico <guilhermao127@gmail.com>
2022-12-07 17:16:31 -0300

    :recycle: refactor: movendo regras de negocio para service

--- agencia_vousuave/src/main/java/com/agencia/vousuave/controller/AuthController.java
+++ agencia_vousuave/src/main/java/com/agencia/vousuave/controller/AuthController.java
@@ -1,18 +1,8 @@
 package com.agencia.vousuave.controller;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.agencia.vousuave.dto.LoginDTO;
 import com.agencia.vousuave.dto.UsuarioDTO;
-import com.agencia.vousuave.entity.Role;
-import com.agencia.vousuave.entity.Usuario;
-import com.agencia.vousuave.enums.ERole;
-import com.agencia.vousuave.repository.RoleRepository;
-import com.agencia.vousuave.repository.UsuarioRepository;
 import com.agencia.vousuave.response.JwtResponse;
-import com.agencia.vousuave.security.jwt.JwtUtils;
-import com.agencia.vousuave.service.impl.UserDetailsImpl;
+import com.agencia.vousuave.service.UsuarioService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -35,63 +19,21 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-	
-	private final AuthenticationManager authenticationManager;
-	private final UsuarioRepository userRepository;
-	private final RoleRepository roleRepository;
-	private final PasswordEncoder encoder;
-	private final JwtUtils jwtUtils;
+
+	private final UsuarioService userSerice;
 
 	@PostMapping("/signin")
 	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginDTO loginDTO) {
 
-		Authentication authentication = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword()));
+		JwtResponse jwtResponse = userSerice.authLogin(loginDTO);
 
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String jwt = jwtUtils.generateJwtToken(authentication);
-
-		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-
-		List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
-				.collect(Collectors.toList());
-
-		return ResponseEntity
-				.ok(new JwtResponse(jwt, userDetails.getId(), userDetails.getNome(), userDetails.getEmail(), roles));
+		return ResponseEntity.ok(jwtResponse);
 	}
 
 	@PostMapping("/signup")
 	public ResponseEntity<?> registerUser(@Valid @RequestBody UsuarioDTO usuarioDTO) {
-		Usuario u = new Usuario(usuarioDTO.getNome(), usuarioDTO.getEmail(), usuarioDTO.getCpf(), usuarioDTO.getCelular(),
-				usuarioDTO.getDataNascimento(), encoder.encode(usuarioDTO.getSenha()));
 
-		Set<String> strRoles = usuarioDTO.getRoles();
-		Set<Role> roles = new HashSet<>();
-
-		if (strRoles == null) {
-			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-			roles.add(userRole);
-		} else {
-			strRoles.forEach(role -> {
-				switch (role) {
-				case "admin":
-					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN) 
-							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-					roles.add(adminRole);
-
-					break;
-
-				default:
-					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-					roles.add(userRole);
-				}
-			});
-		}
-
-		u.setRoles(roles);
-		userRepository.save(u);
+		userSerice.save(usuarioDTO);
 
 		return ResponseEntity.ok("User registered successfully!");
 	}
